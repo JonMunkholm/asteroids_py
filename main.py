6fc2be769ea803500d0dc3b622000bdf03fe0cbc
Jonnythan <jmunkholm1@yahoo.com>
2025-03-11 21:16:34 -0700

    turn left/right

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    hero = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -17,7 +17,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        hero.draw(screen)
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt =  clock.tick(60) / 1000
 
